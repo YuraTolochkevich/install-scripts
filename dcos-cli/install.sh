a27251da41b2e5ad727b74972c980358ed913ba7
Connor Doyle <connor@mesosphere.io>
2015-02-24 10:23:01 -0800

    Make the curl step fail on server errors.

--- dcos-cli/install.sh
+++ dcos-cli/install.sh
@@ -20,12 +20,13 @@ echo "Installing DCOS CLI from wheel...";
 echo "";
 
 BUILD_DIR="/tmp/dcos-cli/build"
+rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
-rm -rf $BUILD_DIR/*
+tree $BUILD_DIR
 pushd $BUILD_DIR
 
 WHEEL_FILE="dcos-0.1.0-py2.py3-none-any.whl"
-curl -O https://downloads.mesosphere.io/dcos-cli/${SECRET}/${WHEEL_FILE}
+curl --fail -O https://downloads.mesosphere.io/dcos-cli/${SECRET}/${WHEEL_FILE}
 pip install ${WHEEL_FILE}
 rm ${WHEEL_FILE}
 
